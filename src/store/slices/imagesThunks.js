9606a74af3f970a9b3601012c4dfcf303b3aa9d3
cruzryan <32502734+cruzryan@users.noreply.github.com>
2022-10-02 08:10:55 -0500

    fixed api in theory

--- src/store/slices/imagesThunks.js
+++ src/store/slices/imagesThunks.js
@@ -1,4 +1,4 @@
-import { collection, addDoc, getDoc, doc } from "firebase/firestore";
+import { collection, addDoc, getDoc, doc, setDoc } from "firebase/firestore";
 import { FireDB } from "../../firebase/firebase.js";
 import { setImages } from "./imagesSlice";
 
@@ -8,15 +8,21 @@ const fetchJSONData = async (URL) => {
 };
 
 const getRandNum = (max) => {
-  return Math.floor(Math.random() * (max - 0 + 1)) + min;
+  return Math.floor(Math.random() * (max - 0 + 1));
 };
+
+function uuidv4() {
+  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  );
+}
 
 export function postUserInput() {
   return async (dispatch, getState) => {
     const { artist, prompt } = getState().images;
     const NasaAPI = `https://images-api.nasa.gov/search?q=${prompt}`;
     const ServerAPI = "https://moonatics.azurewebsites.net/query?data=";
-    const imagesState = {
+    let imagesState = {
       images: [],
       uid: "",
       prompt,
@@ -25,19 +31,26 @@ export function postUserInput() {
       error: false,
     };
 
-    const fetchRes = fetchJSONData(NasaAPI);
+    const fetchResPrev = await fetchJSONData(NasaAPI).then(async data => {
+
+      let fetchRes = data.collection;
+
+    console.log(fetchRes)
+    // let fetchRes = fetchResPre.collection;
 
     for (let i = 0; i < 3; i++) {
       const num = getRandNum(fetchRes.items.length);
       imagesState.images.push({
         originalURL: fetchRes.items[num]?.links[0]?.href,
-        description: fetchRes.items[num]?.data?.description,
+        description: fetchRes.items[num]?.data[0]?.description,
         cachedImage: "",
       });
     }
 
     imagesState.loading = false;
     imagesState.error = false;
+
+    console.log("imgs state: ", imagesState)
     const b64Data = window.btoa(JSON.stringify(imagesState));
 
     try {
@@ -47,17 +60,30 @@ export function postUserInput() {
         obj.cachedImage = resData.images[idx].cachedImage;
       });
 
-      const newDocRef = await addDoc(collection(FireDB, "data"), {
+
+      let docid = uuidv4();
+      const newDocRef = await setDoc(doc(FireDB, "data",  docid), {
         images: imagesState.images,
         userPrompt: prompt,
         artist,
-      });
+        id: docid
+      })
+
+      // const newDocRef = await addDoc(collection(FireDB, "data"), {
+      //   images: imagesState.images,
+      //   userPrompt: prompt,
+      //   artist,
+      // });
 
       dispatch(setImages({ ...imagesState, uid: newDocRef.id }));
+      navigate(`/share=${docid}`);
     } catch (e) {
       imagesState = { images: [], uid: "", artist: artist, prompt, loading: false, error: true };
       dispatch(setImages(imagesState))
+      navigate(`/share=${docid}`);
     }
+
+   });
   };
 }
 
